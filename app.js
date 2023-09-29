8f3fbbdc8a397b78cbffdd05e702b1e5e5fa0fff
Juan Cruz Castillo Martinez <castillom.juancruz@gmail.com>
2023-09-29 20:14:21 -0300

    version v2

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-//const h1 = document.getElementById("titulo")
+const h1 = document.getElementById("titulo")
 
-//h1.innerText = "HOLA MUNDO"
+h1.innerText = "HOLA MUNDO"
 
 console.log("hello world")
